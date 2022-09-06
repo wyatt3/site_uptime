25e7e295442ab5dce72d405f838e28de09571560
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-09-06 10:08:16 -0600

    moved into try/except becuase when the site was down it would error out and not send the discord message

--- ping.py
+++ ping.py
@@ -1,27 +1,32 @@
 import requests
-r = requests.head("https://nyoom.wyattjohnson.net")
-
 url = 'https://discord.com/api/webhooks/1016725902140379287/GVIDVseXQrWGClj7x9gyc_VNm-rN_vQnNcMfzprCUfIuW8tsmndqKDQnb1a7FiZLfREk'
 
 last_check = open(r"last_check.txt", "r")
 up = last_check.read()
 last_check.close()
+try:
+    r = requests.head("https://nyoom.wyattjohnson.net", timeout=2.5)
 
-if r.status_code == 200 and up != "true":
-    message = {
-        "content": "Nyoom is back up",
-        "username": "Uptime Checkorator",
-        "avatar_url": "https://i.imgur.com/M3z7bgZ.gif"
-    }
-    this_check = open(r"last_check.txt", "w")
-    this_check.write("true")
-    requests.post(url, json = message)
-elif r.status_code != 200 and up == "true":
-    message = {
-        "content": "Nyoom is borked",
-        "username": "Downtime Checkorator",
-        "avatar_url": "https://i.imgur.com/sohWhy9.png"
-    }
-    this_check = open(r"last_check.txt", "w")
-    this_check.write("false")
-    requests.post(url, json = message)
+    if r.status_code == 200 and up != "true":
+        this_check = open(r"last_check.txt", "w")
+        this_check.write("true")
+        this_check.close()
+
+        message = {
+            "content": "Nyoom is back up",
+            "username": "Uptime Checkorator",
+            "avatar_url": "https://i.imgur.com/M3z7bgZ.gif"
+        }
+        requests.post(url, json = message)
+except:
+    if up == "true":
+        this_check = open(r"last_check.txt", "w")
+        this_check.write("false")
+        this_check.close()
+
+        message = {
+            "content": "Nyoom is borked",
+            "username": "Downtime Checkorator",
+            "avatar_url": "https://i.imgur.com/sohWhy9.png"
+        }
+        requests.post(url, json = message)
