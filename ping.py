0fcde2c1199833258f38a439d2d67014111db61f
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-09-07 13:55:35 -0600

    added absolute path

--- ping.py
+++ ping.py
@@ -1,14 +1,18 @@
 import requests
+import os
 url = 'https://discord.com/api/webhooks/1016725902140379287/GVIDVseXQrWGClj7x9gyc_VNm-rN_vQnNcMfzprCUfIuW8tsmndqKDQnb1a7FiZLfREk'
 
-last_check = open(r"~/site_uptime/last_check.txt", "r")
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+file_path = absolute_path + "/last_check.txt"
+last_check = open(file_path, "r")
 up = last_check.read()
 last_check.close()
+
 try:
     r = requests.head("https://nyoom.wyattjohnson.net", timeout=2.5)
     print(r.status_code)
     if r.status_code == 200 and up != "true":
-        this_check = open(r"~/site_uptime/last_check.txt", "w")
+        this_check = open(file_path, "w")
         this_check.write("true")
         this_check.close()
 
@@ -22,7 +26,7 @@ except:
     print('site is down')
     if up == "true":
         print('send message')
-        this_check = open(r"~/site_uptime/last_check.txt", "w")
+        this_check = open(file_path, "w")
         this_check.write("false")
         this_check.close()
 
